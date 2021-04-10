6660f1e9670ea8b848de1012936f7f64bcd30fa0
Maria Jaramillo <milagrosmjr20@gmail.com>
2021-04-10 14:16:15 -0400

    https

--- src/helper/getGif.js
+++ src/helper/getGif.js
@@ -1,5 +1,5 @@
 export const getGif = async category => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=iG2ZzbLSGUbkJjOp1d6U582vDPDJUqpT&q=${category}&limit=12`
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=iG2ZzbLSGUbkJjOp1d6U582vDPDJUqpT&q=${category}&limit=12`
 
   const resp = await fetch(url)
   const { data } = await resp.json()
